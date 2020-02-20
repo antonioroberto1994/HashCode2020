96238feea911cc9a8e2774b2926726331ce38a96
Vincenzo Vigilante <info@vvigilante.com>
2020-02-20 19:52:12 +0100

    objs

---
+++ objs.py
@@ -0,0 +1,17 @@
+import sys
+
+class Book():
+    pass
+
+class Library():
+    def __init__(self):
+    def get_score(self, remaining_days):
+        pass
+
+
+def main(fname):
+    pass
+
+
+if "__main__" == __name__:
+    main(sys.argv[1])
