fa0e1f20eed28616173b493b6ad72fe15c2ede03
antonio <a.roberto11@studenti.unisa.it>
2020-02-20 20:34:33 +0100

    script finito

--- algo.py
+++ algo.py
@@ -26,6 +26,12 @@ if __name__ == "__main__":
     data = LoadData('data'+os.sep+'b_read_on.txt')
 
     library_list = data.get_library_list()
+
+    # Per aggiornare velocemente la lista delle librerie rimanenti
+    library_dict = {}
+    for library in library_list:
+        library_dict[library] = None
+
     remaining_days = data.get_days()
     scanned_books = {}
 
@@ -39,8 +45,23 @@ if __name__ == "__main__":
 
         while update_steps != 0:
             # prendo la libreria a massimo guadagno
-            next_library = priority_queue.max()
+            _ , next_library = priority_queue.max()
 
+            # aggiungo per l'output
             output_lib.append(next_library)
 
+            # Aggiorno le librerie da scansionare
+            del library_dict[next_library]
+
+            # Aggiorno i libri scannerizzati
+            update_scanned_books(scanned_books, next_library)
+
+            # update remaining days 
+            remaining_days -= next_library.signup_time
             update_steps -= 1
+
+        # Aggiorno library list 
+        # le chiavi del dizionario sono le librerie che non ho allocato
+        library_list = [library for library in library_dict.keys()]
+
+       
