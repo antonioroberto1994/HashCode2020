f8d538a70b981bb7bb164e9f99d4648a00c54353
antonio <a.roberto11@studenti.unisa.it>
2020-02-20 19:55:38 +0100

    interfaccia 1

--- objs.py
+++ objs.py
@@ -1,12 +1,33 @@
 import sys
 
+
+class LoadData():
+
+    def get_books_scores():
+        pass
+
+    def get_library_list():
+        pass
+
+    def get_days():
+        pass
+
+
 class Book():
     pass
+
 
 class Library():
     def __init__(self):
         pass
+
     def get_score(self, remaining_days):
+        pass
+
+    def get_books():
+        pass
+
+    def get_books_at_day(day,remaining_books):
         pass
 
 
