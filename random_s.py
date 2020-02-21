b653b7d93d51e082761982af20fe09044fc99ff0
antonio <a.roberto11@studenti.unisa.it>
2020-02-21 01:43:57 +0100

    random

---
+++ random_s.py
@@ -0,0 +1,79 @@
+import heapq
+import os
+import sys
+from objs import Library, LoadData
+from max_heap import MaxHeap
+import random
+
+updating_frequency = 100000
+
+
+def update_queue(library_list):
+    remaining_libraries = []
+
+    for library in library_list:
+        remaining_libraries.append((random.random(), library))
+
+    priority_queue = MaxHeap(remaining_libraries)
+
+    return priority_queue
+
+
+def update_scanned_books(scanned_books, library, remaining_days):
+    for book in library.get_books(scanned_books, remaining_days):
+        scanned_books[book] = None
+
+
+if __name__ == "__main__":
+    data = LoadData(sys.argv[1])
+
+    library_list = data.get_library_list()
+
+    # Per aggiornare velocemente la lista delle librerie rimanenti
+    library_dict = {}
+    for library in library_list:
+        library_dict[library] = None
+
+    remaining_days = data.get_days()
+    scanned_books = {}
+
+    output_scanned = []
+    output_lib = []
+
+    while len(library_list) != 0 and remaining_days > 0:
+        # Prendo una versione aggiornata della priority queue
+        priority_queue = update_queue(library_list)
+
+        remaining_update_steps = updating_frequency
+
+        while remaining_update_steps != 0 and len(priority_queue) > 0:
+            # prendo la libreria a massimo guadagno
+            _, next_library = priority_queue.remove_max()
+
+            # aggiungo per l'output
+            output_scanned.append(library_list.copy())
+            output_lib.append(next_library)
+
+            # Aggiorno le librerie da scansionare
+            del library_dict[next_library]
+
+            # Aggiorno i libri scannerizzati
+            update_scanned_books(scanned_books, next_library, remaining_days)
+
+            # update remaining days
+            remaining_days -= next_library.signup_time
+            remaining_update_steps -= 1
+
+        # Aggiorno library list
+        # le chiavi del dizionario sono le librerie che non ho allocato
+        library_list = [library for library in library_dict.keys()]
+
+    outfilename = sys.argv[1].replace(".txt","_output.txt")
+
+    with open(outfilename, "w") as f:
+        f.write("{}\n".format(len(output_lib)))
+
+        for l,s in zip(output_lib, output_scanned):
+            books = l.get_books_in_order(s)
+            f.write("{} {}\n".format(l.id,len(books)))
+            f.write("{}\n".format(' '.join([str(b.id) for b in books])))
